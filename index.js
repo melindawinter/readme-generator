292c644a77d201745e96b93ab10879280d405097
Melinda Winter <melindawinter42@gmail.com>
2020-07-03 13:49:20 -0700

    setting up files

---
+++ index.js
@@ -0,0 +1,19 @@
+// global variables
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,10 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
+
+// ${data.etc} to grab info
