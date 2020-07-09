71b35993c1eb6a2bffca51a04a560d45472fa93d
Melinda Winter <melindawinter42@gmail.com>
2020-07-09 16:16:25 -0700

    adding questions to array

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
+    name: "project",
+    message: "What is the name of your project?",
   },
   {
     type: "list",
     name: "license",
     message: "Which license would you like to use for your project?",
-    choices: ["Apache", "MIT", "GNU"],
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+    ],
   },
 ];
 
@@ -26,7 +30,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-  inquirer.prompt(questions).then(({ github, license }) => {});
+  inquirer.prompt(questions).then(({ project, license }) => {});
 }
 
 // function call to initialize program
